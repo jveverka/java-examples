66215518b6e69856651d5559a1e6d90678a86ab1
Juraj Veverka <juraj.veverka@inter-net.sk>
2017-07-26 06:40:32 +0200

    implemented commandline scenario launching

--- grpc-demo/README.md
+++ grpc-demo/README.md
@@ -1,18 +1,28 @@
-GRPC simple demo
+gRPC simple demo
 ================
-This example implements very simple demo of GRPC integration
-of simple server and client application.
+This example implements very simple demo of [gRPC](https://grpc.io/) communication
+between simple server and client application. Once server is
+started, client connects to the server and can run one of pre-defined
+scenarios. Server always responds to particular client scenario.
 
 Build and Install
-=================
-```gradle clean install```
-```gradle installDist```
-```gradle distZip```
+-----------------
+```gradle clean installDist distZip```  
+```./build/install/grpc-demo/bin/grpc-demo --help```
 
 Run Server
-==========
+----------
+```./build/install/grpc-demo/bin/grpc-demo --port 50051```
 
 
 Run Client
-==========
+----------
+```./build/install/grpc-demo/bin/grpc-demo --host <serverHostName> --port 50051 --scenario <scenarioName>```
+
+Supported client scenarios
+--------------------------
+
+#### sayHello 
+client sends one hello message to server and finish.  
+```./build/install/grpc-demo/bin/grpc-demo --host localhost --port 50051 --scenario sayHello```
 
--- grpc-demo/src/main/java/itx/examples/grpc/service/Main.java
+++ grpc-demo/src/main/java/itx/examples/grpc/service/Main.java
@@ -1,15 +1,68 @@
 package itx.examples.grpc.service;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
 
     final private static Logger LOG = LoggerFactory.getLogger(Main.class);
+    final private static String SAY_HELLO_SCENARIO = "sayHello";
 
-    public static void main(String[] args) {
-        LOG.info("Main");
+    @Parameter(names = {"--host" }, description = "Host name of the server, if specified, runs in client mode.")
+    private String host;
 
+    @Parameter(names = {"--port" }, description = "Port of the server or client.")
+    private int port;
+
+    @Parameter(names = {"--scenario" }, description = "Name of client's scenario to run.")
+    private String scenarioName;
+
+    @Parameter(names = {"--help", "-h", "--info", "-i" }, help = true)
+    private boolean help;
+
+    public static void main(String[] argv) {
+        Main main = new Main();
+        JCommander jCommander = JCommander.newBuilder()
+                .addObject(main)
+                .build();
+        jCommander.parse(argv);
+        try {
+            main.run(jCommander);
+        } catch (Exception e) {
+            LOG.error("Exception: ", e);
+        }
+    }
+
+    public void run(JCommander jCommander) throws Exception {
+        if (help) {
+            jCommander.usage();
+            System.exit(1);
+        }
+        if (host == null) {
+            LOG.info("Starting gRPC server at port {}", port);
+            final SimpleServer server = new SimpleServer(port);
+            server.start();
+            server.blockUntilShutdown();
+            LOG.info("gRPC server stopped.");
+        } else {
+            LOG.info("Starting gRPC client {}:{} with scenario {}", host, port, scenarioName);
+            SimpleClient client = new SimpleClient(host, port);
+            try {
+                switch(scenarioName) {
+                    case SAY_HELLO_SCENARIO:
+                        HelloReply helloReply = client.greet("world");
+                        LOG.info("response from server: {}", helloReply.getMessage());
+                        break;
+                    default:
+                        LOG.error("Unsupported scenario name: {}", scenarioName);
+                }
+            } finally {
+                client.shutdown();
+                LOG.info("gRPC client stopped.");
+            }
+        }
     }
 
 }
